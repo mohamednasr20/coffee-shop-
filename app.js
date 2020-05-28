27af0769f83d14f53deffde934a692202a45c926
mohamednasr20 <mohamednasr8687@gmail.com>
2020-05-28 01:31:15 -0400

    slider js done

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const burger = document.querySelector(".nav__burger");
 const links = document.querySelector(".nav__list");
 const nav = document.querySelector(".nav");
 const social = document.querySelector(".social--mid");
+const slide = document.querySelector(".reviews__slide");
+const prev = document.querySelector(".reviews__prev");
+const next = document.querySelector(".reviews__next");
+let auto = true;
 
 const fixNav = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -16,10 +20,10 @@ const toggleLinks = () => {
   burger.classList.toggle("toggle");
 };
 
-function inViewport(element) {
+const inViewport = (element) => {
   let bb = element.getBoundingClientRect();
   return !(bb.top > innerHeight || bb.bottom < 0);
-}
+};
 
 window.addEventListener("scroll", fixNav);
 
@@ -31,5 +35,49 @@ document.addEventListener("scroll", (e) => {
     setTimeout(() => {
       social.classList.remove("show--icon");
     }, 1000);
+  }
+});
+
+let index = 0;
+let slideInternal;
+
+const nextSlide = () => {
+  if (index === 0) {
+    index++;
+    slide.style.transform = "translate(-100%)";
+    console.log(index);
+  } else {
+    index--;
+    slide.style.transform = "translate(0%)";
+    console.log(index);
+  }
+};
+
+const prevSlide = () => {
+  if (index === 1) {
+    slide.style.transform = "translate(0)";
+    index--;
+  } else {
+    slide.style.transform = "translate(-100%)";
+    index++;
+  }
+};
+
+if (auto) {
+  slideInternal = setInterval(nextSlide, 2000);
+}
+
+next.addEventListener("click", (e) => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInternal);
+    slideInternal = setInterval(nextSlide, 2000);
+  }
+});
+prev.addEventListener("click", (e) => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInternal);
+    slideInternal = setInterval(nextSlide, 2000);
   }
 });

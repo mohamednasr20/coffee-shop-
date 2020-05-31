34bb8a6e9f5100a4d1a6934b4187ba5ed1895db0
mohamednasr20 <mohamednasr8687@gmail.com>
2020-05-30 23:29:08 -0400

    finished

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const burger = document.querySelector(".nav__burger");
-const links = document.querySelector(".nav__list");
+const burger = document.querySelector(".burger");
+const links = document.querySelector(".list");
 const nav = document.querySelector(".nav");
-const social = document.querySelector(".social--mid");
-const slide = document.querySelector(".reviews__slide");
-const prev = document.querySelector(".reviews__prev");
-const next = document.querySelector(".reviews__next");
+const social = document.querySelector(".social-mid");
+const slide = document.querySelector(".slide");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const mq = window.matchMedia("(max-width: 768px)");
+const form = document.querySelector(".contact-form");
+const submit = document.querySelector(".submit");
+const inputs = document.querySelectorAll("input");
+const success = document.querySelector(".success");
+const error = document.querySelectorAll(".error");
+let index = 0;
+let transCount;
+let slideInternal;
 let auto = true;
 
 const fixNav = () => {
@@ -25,47 +34,54 @@ const inViewport = (element) => {
   return !(bb.top > innerHeight || bb.bottom < 0);
 };
 
+const nextSlide = () => {
+  if (!mq.matches && index > 2) {
+    index = -1;
+  }
+  mq.matches ? (transCount = -100) : (transCount = -33.3);
+  index < 5 ? index++ : (index = 0);
+  slide.style.transform = `translate(${index * transCount}%)`;
+};
+
+const prevSlide = () => {
+  if (!mq.matches && index === 0) {
+    index = 4;
+  }
+  mq.matches ? (transCount = -100) : (transCount = -33.3);
+  index > 0 ? index-- : (index = 5);
+  slide.style.transform = `translate(${index * transCount}%)`;
+};
+
+if (auto) {
+  slideInternal = setInterval(nextSlide, 2000);
+}
+
+const sendMessage = () => {
+  inputs.forEach((input, i) => {
+    if (input.value === "") {
+      success.classList.remove("show");
+      error[i].classList.add("show");
+      setTimeout(() => {
+        error[i].classList.remove("show");
+      }, 3000);
+    } else {
+      success.classList.add("show");
+    }
+  });
+};
+
 window.addEventListener("scroll", fixNav);
 
 burger.addEventListener("click", toggleLinks);
 
 document.addEventListener("scroll", (e) => {
   if (inViewport(social)) {
-    social.classList.add("show--icon");
+    social.classList.add("show-icon");
     setTimeout(() => {
-      social.classList.remove("show--icon");
+      social.classList.remove("show-icon");
     }, 1000);
   }
 });
-
-let index = 0;
-let slideInternal;
-
-const nextSlide = () => {
-  if (index === 0) {
-    index++;
-    slide.style.transform = "translate(-100%)";
-    console.log(index);
-  } else {
-    index--;
-    slide.style.transform = "translate(0%)";
-    console.log(index);
-  }
-};
-
-const prevSlide = () => {
-  if (index === 1) {
-    slide.style.transform = "translate(0)";
-    index--;
-  } else {
-    slide.style.transform = "translate(-100%)";
-    index++;
-  }
-};
-
-if (auto) {
-  slideInternal = setInterval(nextSlide, 2000);
-}
 
 next.addEventListener("click", (e) => {
   nextSlide();
@@ -80,4 +96,9 @@ prev.addEventListener("click", (e) => {
     clearInterval(slideInternal);
     slideInternal = setInterval(nextSlide, 2000);
   }
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
 });
